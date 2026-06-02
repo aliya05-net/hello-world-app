f1df7cbcae1a07e53daa055b576e2807da7fce53
aliya05-net <aliyamin2005@gmail.com>
2026-06-02 16:57:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function setGreen() {
+    document.getElementById("helloText").style.color = "green";
+}
+
+function setOrange() {
+    document.getElementById("helloText").style.color = "orange";
+}
